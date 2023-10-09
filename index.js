6adca80dd749bc33f66a877629c67b1f77133cfc
carlbrgs <carlbrgs77@gmail.com>
2023-10-09 17:40:46 +0200

    Added footer and pop-up

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 //HOME PAGE
 
+function fermerPopup() {
+    let page = document.querySelector(".page-content");
+    let close = document.querySelector(".pop-up");
+    close.style.display = "none";
+    removeFilter = page.style.filter = "none";
+    localStorage.setItem('popup', 'false')
+}
+
+let popup = document.querySelector(".pop-up");
+let test = localStorage.getItem('popup')
+console.log(test)
+
+if(test == 'false'){
+    let page = document.querySelector(".page-content");
+    let close = document.querySelector(".pop-up");
+    close.style.display = "none";
+    removeFilter= page.style.filter = "none";
+    
+}
+
 //PRODUCT PAGE
 
