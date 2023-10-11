a52e1aa0937e03746c80a24ed117e41f5df4a575
JulienBNT <julien.binet91@gmail.com>
2023-10-11 15:54:37 +0200

    registerpage in progress, update connexion.php

--- connexion.php
+++ connexion.php
@@ -1,11 +1,25 @@
 <?php
 
-$connexion = new mysqli("localhost", "root", "", "myDB");
+// $connexion = new mysqli("localhost", "root", "", "myDB");
 
-if (!$connexion -> connect_error){
-    printf("Erreur : Connexion impossible : ". $connexion->connect_error);
-    exit();
+// if (!$connexion -> connect_error){
+//     printf("Erreur : Connexion impossible : ". $connexion->connect_error);
+//     exit();
+// }
+// printf("Succès de la connexion !");
+
+$host = "localhost";
+$port = "3306";
+$dbname = "myDB";
+$usermane = "root";
+$password = "pass";
+
+try {
+    $conn = new PDO('mysql:host='.$host.';port='.$port.';dbname='.$dbname, $username, $password);
 }
-printf("Succès de la connexion !");
+catch(PDOException $except){
+    printf("Echec de la connexion");
+}
+
 
 ?>
