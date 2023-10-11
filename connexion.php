9842790fe696eba99d6f35c78f6d1e070777ea4b
carlbrgs <carlbrgs77@gmail.com>
2023-10-11 17:21:54 +0200

    minor changes connexion

--- connexion.php
+++ connexion.php
@@ -1,8 +1,8 @@
 <?php
 
-$connexion = new mysqli("localhost", "root", "", "bddsite");
+$mysqli = new mysqli("localhost", "root", "", "bddsite");
 
-if ($connexion -> connect_error){
+if ($mysqli -> connect_error){
     printf("Erreur : Connexion impossible : ". $connexion->connect_errno);
     exit();
 }
@@ -19,10 +19,4 @@ if ($connexion -> connect_error){
 // catch(PDOException $except){
 //     printf("Echec de la connexion");
 // }
-<<<<<<< HEAD
-// printf("Succès de la connexion !");
-mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-$mysqli = new mysqli("localhost", "root", "", "bddsite");
-=======
->>>>>>> 126af18c6a9b5570018865efba0de8e4e1ace469
 ?>
