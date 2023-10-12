ba385b45d4be1d72c36de7f7524c931a5621b429
JulienBNT <julien.binet91@gmail.com>
2023-10-12 10:36:12 +0200

    js issue corrected

--- connexion.php
+++ connexion.php
@@ -2,10 +2,16 @@
 
 $mysqli = new mysqli("localhost", "root", "", "bddsite");
 
-if ($mysqli -> connect_error){
+if ($mysqli -> connect_error){  
     printf("Erreur : Connexion impossible : ". $connexion->connect_errno);
     exit();
 }
+
+$createTableUsers = "CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    pseudo VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL)";
 
 // $host = "localhost";
 // $port = "3306";
--- index.js
+++ index.js
@@ -25,18 +25,18 @@ if(test == 'false'){
     
 }
 
-//PRODUCT PAGE
-let x = document.getElementsByClassName('product');
+// //PRODUCT PAGE
+// let x = document.getElementsByClassName('product');
 
-function getVal() {
-    const searchbar = document.getElementById('searchbar').value;
-    for (i = 0; i < x.length; i++) { 
-        if (!x[i].innerHTML.toLowerCase().includes(searchbar)) {
-            x[i].style.display="none";
-        }
-        else {
-            x[i].style.display="list-item";                 
-        }
-    }
-}
+// function getVal() {
+//     const searchbar = document.getElementById('searchbar').value;
+//     for (i = 0; i < x.length; i++) { 
+//         if (!x[i].innerHTML.toLowerCase().includes(searchbar)) {
+//             x[i].style.display="none";
+//         }
+//         else {
+//             x[i].style.display="list-item";                 
+//         }
+//     }
+// }
 
